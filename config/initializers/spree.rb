4d5307dc4641a0976f3fb7d3b8c22654dbc97a63
Trevor <trevorhoward2@hotmail.com>
2017-06-19 14:05:45 -0600

    Finally fixed logo loading issue

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -28,16 +28,16 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  config.logo = "/assets/images/123.jpg"
+  config.logo = "logo.png"
 
   # Template to use when rendering layout
-  config.layout = "spree/layouts/spree_application"
+  config.layout = "application"
 
 
   # Admin:
 
   # Custom logo for the admin
-  config.admin_interface_logo = "images/123.jpg"
+  config.admin_interface_logo = "logo.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
